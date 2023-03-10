2311196df1edb108b9f6cedbe3e8db3d7c2c70e8
Silas <silasfas@hotmail.com>
2023-03-10 20:31:27 -0300

    useState na pasta form

--- epsode-1/src/components/Form.js
+++ epsode-1/src/components/Form.js
@@ -1,9 +1,16 @@
+import { useState } from "react" // pacote que já vem com react
+
 function Form() {
 
-    function cadastrarUsuario(event) {
-        event.preventDefault()
-        console.log('Tudo okay')
+    function cadastrarUsuario(e) {
+        e.preventDefault()
+        console.log(name) // name: para resgatar o valor
+        console.log(password) // password: para resgatar o valor
+        console.log(`Usuário ${name} foi cadastro com a senha ${password}.`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <>
@@ -11,7 +18,26 @@ function Form() {
 
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome:" />
+                    <label htmlFor="name">Name: </label>
+                    <input
+                        type="text"
+                        id='name'
+                        name='name'
+                        placeholder="Digite o seu nome:"
+                        onChange={(e) => setName(e.target.value)}
+                    // setName para atribuir o valor
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite a sua senha:"
+                        onChange={(e) => setPassword(e.target.value)}
+                    // setPassword para atribuir o valor
+                    />
                 </div>
                 <br></br>
                 <div>
@@ -21,5 +47,4 @@ function Form() {
         </>
     )
 }
-
 export default Form
