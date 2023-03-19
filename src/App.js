104a61fe6491038ba260c7525c3be3497c0a4f49
Silas <silasfas@hotmail.com>
2023-03-19 16:14:48 -0300

    conditional redering and CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import './Index.css';
 
 function App() {
+
+  const age = 18
+  const isGreen = true
+
   return (
     <div className="App">
-      <h1>Hello there! Start here!</h1>
+      {age >= 18 ? <h1>over age</h1> : <h1>under age</h1>}
+    <h1 style={{color: isGreen ? 'green' : 'red'}}>This has color</h1>
+
+    {isGreen && <button>This is a button</button>} 
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// conditional in react
+// {age >= 18 ? <h1>over age</h1> : <h1>underage</h1>}
+
+/* conditional in react
+function App() {
+
+  const age = 15
+
+  if (age >= 18) {
+    return <h1>over age</h1>
+  }
+  return (
+    <div className="App">
+      <h1>Under age</h1>
+    </div>
+  );
+}
+*/
