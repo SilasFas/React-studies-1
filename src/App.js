f80b5d66378bb508921cb2e4f1284288465bce49
Silas <silasfas@hotmail.com>
2023-03-21 16:18:12 -0300

    'useState1'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [age, setAge] = useState(0)
+
+  const increaseAge = () => {
+    setAge(age + 1)
+    console.log(age)
+  }
+  const decrease = () => {
+    setAge(age - 1)
+  }
+
   return (
     <div className="App">
-      <h1>Hello there! Start here!</h1>
+      <h1>{age}</h1>
+      <button onClick={increaseAge}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
     </div>
   );
 }
