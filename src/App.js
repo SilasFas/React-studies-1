c07a93593e4147b056752e3148f9057746e1ef72
Silas <silasfas@hotmail.com>
2023-03-22 02:56:31 -0300

    'cases'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,84 @@ import { useState } from "react";
 
 function App() {
 
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div className="App">
+
+      <h1>{counter}</h1>
+      <button onClick={() => { setCounter(counter + 1) }}>Increase</button>
+      <button onClick={() => { setCounter(0) }}>Set to Zero</button>
+      <button onClick={() => { setCounter(counter - 1) }}>Decrease</button>
+    </div >
+  );
+}
+
+export default App;
+
+/* **>> Example 4 <<**
+
+import './App.css';
+import { useState } from "react";
+
+function App() {
+
+  const [textcolor, setTextColor] = useState('black')
+
+  return (
+    <div className="App">
+
+      <button
+        onClick={() => { setTextColor(textcolor === 'black' ? 'red ' : 'black') }}>
+        Change the color
+      </button>
+
+      <h1 style={{ color: textcolor }}>Hi, my name is Silas!</h1>
+    </div >
+  );
+}
+
+export default App;
+
+*/
+
+//**>> Example 3 <<**
+// import './App.css';
+// import { useState } from "react";
+
+// function App() {
+
+//   const [showText, setShowText] = useState(true)
+
+//   // const inputText = () => {
+//   //   setShowText(!showText)
+//   // }
+
+//   return (
+//     <div className="App">
+
+//       <button
+//         onClick={() => setShowText(!showText)}>
+//         Show/Hide
+//       </button>
+
+//       {showText === true && <h1>Hi, my name is Silas!</h1>
+//         /* if shoWText is equal to true than show this >>>
+//         {showText && <h1>Hi, my name is Silas!</h1>}*/
+//       }
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/* **>> Example 2 <<**
+
+import './App.css';
+import { useState } from "react";
+
+function App() {
+
   const [inputValue, setInputValue] = useState('')
 
   const handleInputChange = (event) => {
@@ -25,11 +103,10 @@ function App() {
 }
 
 export default App;
+*/
 
+/* **>> Example 1 <<**
 
-
-
-/*
 import './App.css';
 import { useState } from "react";
 
