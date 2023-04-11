f8d955cd412a5cb1cd5910f2d03a54eaf9ca54f0
Silas <silasfas@hotmail.com>
2023-04-11 12:27:32 -0300

    update

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ function App() {
   const Job = (props) => {
     return (
       <div>
-        <h1>{props.salary}
-        </h1> <h1>{props.position}</h1>
+        <h1>{props.salary}</h1>
+        <h1>{props.position}</h1>
         <h1>{props.company}</h1>
       </div>
     )
