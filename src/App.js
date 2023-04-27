b74073f48d30aa8141a9a8c9f5bda0b4db32e837
Silas <silasfas@hotmail.com>
2023-04-27 20:12:40 -0300

    Cat API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import './App.css';
+import axios, { Axios } from 'axios';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  const [catFact, setcatFact] = useState("")
+
+
+  const fetchCatFact = () => {
+    fetch("https://catfact.ninja/fact")
+      .then((response) => response.json())
+      .then((data) => {
+        setcatFact(data.fact)
+      })
+  }
+
+  useEffect(() => {
+    fetchCatFact()
+  }, [])
+
+  // Axios.length("https://catfact.ninja/fact")
+  //   .then((response) => {
+  //     console.log(response.data)
+  //   })
+
   return (
     <div className="App">
-      <h1>Hello there! Start here!</h1>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
