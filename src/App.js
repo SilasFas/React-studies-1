258bb58f5c059377fb609f7544cc9178c81d44a1
Silas <silasfas@hotmail.com>
2023-04-28 01:27:09 -0300

    different pages using Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+import { Navbar } from './pages/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+      </Router>
       <h1>Hello there! Start here!</h1>
     </div>
   );
