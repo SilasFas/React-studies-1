49515760640121f7eed58100610e4326f0517267
Silas <silasfas@hotmail.com>
2023-04-27 23:49:14 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [text, setText] = useState("")
+
+
+  const handleClick = (endpoint) => {
+    fetch(`https://excuser-three.vercel.app/v1/excuse/${endpoint}/`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[0].excuse)
+        setText(data[0].excuse)
+      })
+  }
+
   return (
     <div className="App">
-      <h1>Hello there! Start here!</h1>
+      <h1>Generate an excuse</h1>
+
+      <button onClick={() => handleClick('party')}>Party</button>
+      <button onClick={() => handleClick('family')}>Family</button>
+      <button onClick={() => handleClick('office')}>Office</button>
+
+      <div>{text}</div>
     </div>
   );
 }
