ba0c8a409d2b33604ca0cb1a26693f226d5d9eac
Silas <silasfas@hotmail.com>
2023-04-29 19:02:48 -0300

    reactQuery

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import { Contact } from './pages/Contact';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { Navbar } from './pages/Navbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      }
+    }
+  })
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+
 export const Home = () => {
-    return <h1>This is the homepage</h1>
-}
+    const {
+        data: catData,
+        isLoading,
+        refetch
+    } = useQuery(['cat'], () => {
+        return fetch("https://catfact.ninja/fact")
+            .then((response) => response.json())
+            .then((fact) => {
+                return fact;
+            });
+    });
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+    return <h1>
+        This is the homepage
+        <p>{catData?.fact}</p>
+        <button onClick={refetch} >Update data</button>
+    </h1>;
+};
